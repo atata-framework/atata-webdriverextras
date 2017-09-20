eaedc84c4c221d3d6f7ca4f25d5341a8079558f6
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-20 16:06:41 +0300

    Update AssemblyInfo

--- src/Atata.WebDriverExtras/Properties/AssemblyInfo.cs
+++ src/Atata.WebDriverExtras/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Yevgeniy Shunevych")]
 [assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2016")]
+[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2017")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyFileVersion("0.11.0")]
+[assembly: AssemblyVersion("0.12.0")]
+[assembly: AssemblyFileVersion("0.12.0")]
