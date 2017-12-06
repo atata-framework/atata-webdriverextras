50acc985d51445d520824e84ab2cd6e94f04b52b
YevgeniyShunevych <eshunevich@gmail.com>
2017-12-06 12:23:51 +0200

    Add Atata.WebDriverExtras.Tests project

---
+++ src/Atata.WebDriverExtras.Tests/UITestFixture.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace Atata.WebDriverExtras.Tests
+{
+    [TestFixture]
+    public abstract class UITestFixture
+    {
+        private const string BaseUrl = "http://localhost:57439/";
+
+        protected RemoteWebDriver Driver { get; private set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Close();
+        }
+
+        protected RemoteWebDriver GoTo(string relativeUrl)
+        {
+            Driver.Navigate().GoToUrl(BaseUrl + relativeUrl);
+            return Driver;
+        }
+    }
+}
