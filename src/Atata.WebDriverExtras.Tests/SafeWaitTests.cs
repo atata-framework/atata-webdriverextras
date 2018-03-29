a766d96b17dcd915c7cb0e20945af8436c90106f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-29 13:32:58 +0300

    Add tests: SafeWait_Success_Immediate, SafeWait_Timeout, SafeWait_PollingInterval and SafeWait_PollingInterval_GreaterThanTimeout

---
+++ src/Atata.WebDriverExtras.Tests/SafeWaitTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Atata.WebDriverExtras.Tests
+{
+    [TestFixture]
+    public class SafeWaitTests
+    {
+        private SafeWait<object> wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            wait = new SafeWait<object>(new object())
+            {
+                Timeout = TimeSpan.FromSeconds(.3),
+                PollingInterval = TimeSpan.FromSeconds(.05)
+            };
+        }
+
+        [Test]
+        public void SafeWait_Success_Immediate()
+        {
+            using (StopwatchAsserter.Within(0, .01))
+                wait.Until(_ =>
+                {
+                    return true;
+                });
+        }
+
+        [Test]
+        public void SafeWait_Timeout()
+        {
+            using (StopwatchAsserter.Within(.3, .01))
+                wait.Until(_ =>
+                {
+                    return false;
+                });
+        }
+
+        [Test]
+        public void SafeWait_PollingInterval()
+        {
+            using (StopwatchAsserter.Within(.3, .01))
+                wait.Until(_ =>
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(.1));
+                    return false;
+                });
+        }
+
+        [Test]
+        public void SafeWait_PollingInterval_GreaterThanTimeout()
+        {
+            wait.PollingInterval = TimeSpan.FromSeconds(1);
+
+            using (StopwatchAsserter.Within(.3, .01))
+                wait.Until(_ =>
+                {
+                    return false;
+                });
+        }
+    }
+}
