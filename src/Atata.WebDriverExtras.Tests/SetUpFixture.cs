b559efec07113a251ec3ab21506e3f31967d8a3d
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2018-09-10 11:25:49 +0300

    Add SetUpFixture to Atata.WebDriverExtras.Tests project with functionality to run Atata.WebDriverExtras.TestApp site on tests startup

---
+++ src/Atata.WebDriverExtras.Tests/SetUpFixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+using NUnit.Framework;
+using Retry;
+
+namespace Atata.WebDriverExtras.Tests
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        private Process coreRunProcess;
+
+        [OneTimeSetUp]
+        public void GlobalSetUp()
+        {
+            try
+            {
+                PingTestApp();
+            }
+            catch
+            {
+                RunTestApp();
+            }
+        }
+
+        private void RunTestApp()
+        {
+            coreRunProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c dotnet run",
+                    WorkingDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\Atata.WebDriverExtras.TestApp")
+                }
+            };
+
+            coreRunProcess.Start();
+
+            Thread.Sleep(5000);
+
+            RetryHelper.Instance.Try(() => PingTestApp()).
+                WithTryInterval(1000).
+                WithTimeLimit(40000).
+                UntilNoException();
+        }
+
+        private WebResponse PingTestApp() =>
+            WebRequest.CreateHttp(UITestFixture.BaseUrl).GetResponse();
+
+        [OneTimeTearDown]
+        public void GlobalTearDown()
+        {
+            coreRunProcess?.CloseMainWindow();
+        }
+    }
+}
