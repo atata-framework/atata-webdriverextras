c0b29865fc00d15a8e50a79bcafe0048b1bc22aa
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-03-27 13:11:08 +0200

    Add AppendSpace extension method for StringBuilder

---
+++ src/Atata.WebDriverExtras/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Atata
+{
+    /// <summary>
+    /// Provides a set of extension methods for <see cref="StringBuilder"/>.
+    /// </summary>
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Appends the space character.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns>A reference to the same <see cref="StringBuilder"/> instance after the append operation has completed.</returns>
+        public static StringBuilder AppendSpace(this StringBuilder builder)
+        {
+            builder.CheckNotNull(nameof(builder));
+
+            return builder.Append(' ');
+        }
+    }
+}
