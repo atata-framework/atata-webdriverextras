801d59c8e6c4531b9963ad038525362c5c412455
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-03-27 12:59:46 +0200

    #23 Add ToShortIntervalString and ToLongIntervalString extension methods for TimeSpan; Add tests: TimeSpanExtensions_ToShortIntervalString and TimeSpanExtensions_ToLongIntervalString

---
+++ src/Atata.WebDriverExtras.Tests/TimeSpanExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Atata.WebDriverExtras.Tests
+{
+    [TestFixture]
+    public class TimeSpanExtensionsTests
+    {
+        [TestCase(0d, ExpectedResult = "0s")]
+        [TestCase(15d, ExpectedResult = "15s")]
+        [TestCase(15.0001d, ExpectedResult = "15s")]
+        [TestCase(15.12d, ExpectedResult = "15.12s")]
+        [TestCase(15.12345d, ExpectedResult = "15.123s")]
+        [TestCase(15.1239d, ExpectedResult = "15.124s")]
+        [TestCase(60d, ExpectedResult = "1m")]
+        [TestCase(60.01d, ExpectedResult = "1m 0.01s")]
+        [TestCase(152d, ExpectedResult = "2m 32s")]
+        [TestCase(152.9d, ExpectedResult = "2m 32.9s")]
+        public string TimeSpanExtensions_ToShortIntervalString(double value)
+        {
+            return TimeSpan.FromSeconds(value).ToShortIntervalString();
+        }
+
+        [TestCase(0d, ExpectedResult = "0.000s")]
+        [TestCase(15d, ExpectedResult = "15.000s")]
+        [TestCase(15.0001d, ExpectedResult = "15.000s")]
+        [TestCase(15.12d, ExpectedResult = "15.120s")]
+        [TestCase(15.12345d, ExpectedResult = "15.123s")]
+        [TestCase(15.1239d, ExpectedResult = "15.124s")]
+        [TestCase(60d, ExpectedResult = "1m 0.000s")]
+        [TestCase(60.01d, ExpectedResult = "1m 0.010s")]
+        [TestCase(152d, ExpectedResult = "2m 32.000s")]
+        [TestCase(152.9d, ExpectedResult = "2m 32.900s")]
+        public string TimeSpanExtensions_ToLongIntervalString(double value)
+        {
+            return TimeSpan.FromSeconds(value).ToLongIntervalString();
+        }
+    }
+}
---
+++ src/Atata.WebDriverExtras/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace Atata
+{
+    /// <summary>
+    /// Provides a set of extension methods for <see cref="TimeSpan"/>.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        /// Converts the value of the <see cref="TimeSpan"/> object to its equivalent short interval string representation.
+        /// For example, 100 milliseconds value converts to "0.1s".
+        /// </summary>
+        /// <param name="value">The <see cref="TimeSpan"/> value.</param>
+        /// <returns>The short interval string representation of the <see cref="TimeSpan"/> value.</returns>
+        public static string ToShortIntervalString(this TimeSpan value)
+        {
+            return ToIntervalString(value, "FFF", false);
+        }
+
+        /// <summary>
+        /// Converts the value of the <see cref="TimeSpan"/> object to its equivalent long interval string representation.
+        /// For example, 100 milliseconds value converts to "0.100s".
+        /// </summary>
+        /// <param name="value">The <see cref="TimeSpan"/> value.</param>
+        /// <returns>The long interval string representation of the <see cref="TimeSpan"/> value.</returns>
+        public static string ToLongIntervalString(this TimeSpan value)
+        {
+            return ToIntervalString(value, "fff", true);
+        }
+
+        private static string ToIntervalString(TimeSpan value, string millisecondsFormat, bool secondsRequired)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            double seconds;
+
+            if (value.TotalMinutes >= 1d)
+            {
+                double minutes = Math.Floor(value.TotalMinutes);
+
+                builder.AppendFormat("{0:F0}m", minutes);
+
+                seconds = value.TotalSeconds - (minutes * 60d);
+            }
+            else
+            {
+                seconds = value.TotalSeconds;
+            }
+
+            if (secondsRequired || seconds > 0 || builder.Length == 0)
+            {
+                if (builder.Length > 0)
+                    builder.AppendSpace();
+
+                builder.Append(Math.Floor(seconds).ToString("F0"));
+
+                string millisecondsString = value.ToString(millisecondsFormat);
+
+                if (millisecondsString.Length > 0)
+                    builder.Append('.').Append(millisecondsString);
+
+                builder.Append('s');
+            }
+
+            return builder.ToString();
+        }
+    }
+}
