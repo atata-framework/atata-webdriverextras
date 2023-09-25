b004c02e24047c0535b8c0f3509e55f83327fee4
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-09-25 17:45:46 +0200

    #55 Replace `NotMissingElementException` with new `ElementNotMissingException`

--- src/Atata.WebDriverExtras/Exceptions/ElementNotMissingException.cs
+++ src/Atata.WebDriverExtras/Exceptions/ElementNotMissingException.cs
@@ -3,6 +3,9 @@ using System.Runtime.Serialization;
 
 namespace Atata
 {
+    /// <summary>
+    /// The exception that is thrown during a waiting for a particular element to be missing, but it was present.
+    /// </summary>
     [Serializable]
     public class ElementNotMissingException : Exception
     {
